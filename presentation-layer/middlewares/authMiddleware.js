c06eb3076864c161937ac04e3df5a4c9c7b0395e
BerkiManman <berkayguler7@hotmail.com>
2024-03-07 10:22:47 +0300

    refactor: removed redundant constant

--- presentation-layer/middlewares/authMiddleware.js
+++ presentation-layer/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ export const authMiddleware = (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
             if (err) {
                 return res.status(400).json({ error: 'Invalid token' });
             }
