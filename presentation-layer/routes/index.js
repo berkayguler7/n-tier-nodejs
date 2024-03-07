22875a9cfcd8ea6dc259a5247073900dc4b9cab9
BerkiManman <berkayguler7@hotmail.com>
2024-03-07 10:18:50 +0300

    implemented weather api call

--- presentation-layer/routes/index.js
+++ presentation-layer/routes/index.js
@@ -7,5 +7,10 @@ const router = Router();
 
 router.use('/users', userRoutes);
 router.use('/items', authMiddleware, itemRoutes);
+router.use('/weather', authMiddleware, async (req, res) => {
+    let weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=Izmir`);
+    weather = await weather.json();
+    res.json(weather);
+});
 
 export default router;
