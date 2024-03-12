9a9f655457edf71dec033deef4613c0364b5220e
BerkiManman <berkayguler7@hotmail.com>
2024-03-12 12:27:11 +0300

    fix: fixed a bug where auth middleware was invoked twice

--- presentation-layer/routes/weatherRoutes.js
+++ presentation-layer/routes/weatherRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
 import weatherController from '../controllers/weatherController.js';
 
 const router = Router();
 
-router.get('/', authMiddleware, weatherController.getWeather);
+router.get('/', weatherController.getWeather);
 
 export default router;
